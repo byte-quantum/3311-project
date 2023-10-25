c76d788aadace37a9b27b472d3c41525fe9faf06
chanuthi <vidara54@gmail.com>
2023-10-25 04:02:35 -0500

    modified:   app/education/page.tsx

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -15,25 +15,25 @@ export default function EducationPage() {
   return (
     <>
       <div className="flex flex-row absolute left-[300px] top-[147px] mx-auto h-auto w-auto gap-x-16 left-500 ">
-        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110  ">
+        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
           <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
         </div>
-        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative duration-700 hover:-translate-y-1 hover:scale-110">
+        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
           <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
         </div>
-        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative duration-700 hover:-translate-y-1 hover:scale-110">
+        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
           <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
         </div>
-        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative duration-700 hover:-translate-y-1 hover:scale-110">
+        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
           <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
         </div>
-        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative duration-700 hover:-translate-y-1 hover:scale-110">
+        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
           <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
         </div>
-        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative duration-700 hover:-translate-y-1 hover:scale-110">
+        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
           <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
         </div>
-        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative duration-700 hover:-translate-y-1 hover:scale-110">
+        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
           <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
         </div>
 
