b36f8ef81c183715979dda6e8cdfcb9bdd9a7065
chanuthi <Vidara54@gmail.com>
2023-11-06 00:46:10 -0600

    Changes to Education Page.tsx
    Drag scroll feature

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useState } from "react";
 import fintip from "@/public/financetips.svg";
 import Image from "next/image";
 import { Terminal } from "lucide-react";
@@ -11,30 +12,117 @@ import {
 } from "@/components/ui/accordion";
 
 
+
+
+
 export default function EducationPage() {
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [prevPercentage, setPrevPercentage] = useState(0);
+  const [percentage, setPercentage] = useState(0);
+
+  useEffect(() => {
+    const track = document.getElementById("image-track");
+
+    const handleOnDown = (e: MouseEvent | TouchEvent) => {
+      setIsMouseDown(true);
+      if (track) {
+        track.dataset.mouseDownAt = e instanceof MouseEvent ? e.clientX.toString() : e.touches[0].clientX.toString();
+      }
+    };
+
+    const handleOnUp = () => {
+      setIsMouseDown(false);
+      if (track) {
+        setPrevPercentage(parseFloat(track.dataset.percentage || "0"));
+      }
+    };
+
+    const handleOnMove = (e: MouseEvent | TouchEvent) => {
+      if (!isMouseDown || !track || !track.dataset.mouseDownAt || track.dataset.mouseDownAt === "0") return;
+
+      const mouseDelta = parseFloat(track.dataset.mouseDownAt) - (e instanceof MouseEvent ? e.clientX : e.touches[0].clientX);
+      const maxDelta = window.innerWidth / 2;
+
+      const percentageDelta = (mouseDelta / maxDelta) * -100;
+      const nextPercentageUnconstrained = prevPercentage + percentageDelta;
+      const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+      setPercentage(nextPercentage);
+
+      track.dataset.percentage = nextPercentage.toString();
+
+      track.animate(
+        {
+          transform: `translateX(${nextPercentage}%)`
+        },
+        { duration: 1200, fill: "forwards" }
+      );
+
+      const images = track.getElementsByClassName("image");
+      for (let i = 0; i < images.length; i++) {
+        const image = images[i];
+        image.animate(
+          {
+            objectPosition: `${100 + nextPercentage}% center`,
+          },
+          { duration: 1200, fill: "forwards" }
+        );
+      }
+    };
+
+    window.addEventListener("mousedown", handleOnDown);
+    window.addEventListener("touchstart", handleOnDown);
+    window.addEventListener("mouseup", handleOnUp);
+    window.addEventListener("touchend", handleOnUp);
+    window.addEventListener("mousemove", handleOnMove);
+    window.addEventListener("touchmove", handleOnMove);
+
+    return () => {
+      window.removeEventListener("mousedown", handleOnDown);
+      window.removeEventListener("touchstart", handleOnDown);
+      window.removeEventListener("mouseup", handleOnUp);
+      window.removeEventListener("touchend", handleOnUp);
+      window.removeEventListener("mousemove", handleOnMove);
+      window.removeEventListener("touchmove", handleOnMove);
+    };
+  }, [isMouseDown, prevPercentage]);
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "visible";
+    };
+  }, []);
+  
+
   return (
     <>
-      <div className="flex flex-row absolute left-[300px] top-[147px] mx-auto h-auto w-auto gap-x-16 left-500 ">
-        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
-          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
+      <div className="flex flex-row absolute left-[500px] top-[147px] mx-auto h-auto w-auto gap-x-16 left-500 " id="image-track" data-mouse-down-at="0" data-percentage={percentage} data-prev-percentage={prevPercentage}>
+        <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110" title="Fintip Image">
+          <h1 className="mb-4 mx-auto text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-whiter">We invest in the world's potential</h1>
+          <p className="mx-auto text-base text-gray-500 md:text-lg lg:text-xl dark:text-gray-400">
+          We are a team of passionate investors, 
+          engineers, and designers that believe in the power of the stock market. 
+          We are here to help you learn how to invest and grow your wealth.</p>
+          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0 " draggable="false" />
         </div>
         <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
-          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
+          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" draggable="false"/>
         </div>
         <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
-          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
+          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" draggable="false" />
         </div>
         <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
-          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
+          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" draggable="false"/>
         </div>
         <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
-          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
+          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" draggable="false"/>
         </div>
         <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
-          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
+          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" draggable="false"/>
         </div>
         <div className="mx-auto bg-red-400 w-[1045px] h-[677px] rounded-3xl relative transition ease-in-out duration-700 hover:-translate-y-1 hover:scale-110">
-          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" />
+          <Image src={fintip} alt="Fintip" layout="fill" objectFit="cover" className="rounded-3xl transiton-opacity ease-out-in duration-700 hover:opacity-0" draggable="false" />
         </div>
 
       </div>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -169,127 +169,127 @@ export default function Sidebar({ children }: SidebarProps) {
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition.Root>
+                          </div>
+                        </Dialog>
+                      </Transition.Root>
 
-        {/* Static sidebar for desktop */}
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-          {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto px-6 border-r border-slate-800">
-            <div className="flex items-center pt-4">
-              <span className="text-2xl font-bold text-green-700">
-                Dorm Dollars
-              </span>
-            </div>
-            <nav className="flex flex-1 flex-col">
-              <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                <li>
-                  <ul role="list" className="-mx-2 space-y-1">
-                    {navigation.map((item) => (
-                      <li key={item.name}>
-                        <a
-                          href={item.href}
-                          className={classNames(
-                            item.href === pathname
-                              ? "bg-slate-900 text-green-500"
-                              : "text-white hover:text-green-500 hover:bg-slate-900",
-                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                          )}
+                      {/* Static sidebar for desktop */}
+                      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+                        {/* Sidebar component, swap this element with another sidebar if you like */}
+                        <div className="flex grow flex-col gap-y-5 overflow-y-auto px-6 border-r border-slate-800 bg-gray-900">
+                          <div className="flex items-center pt-4">
+                            <span className="text-2xl font-bold text-green-700">
+                              Dorm Dollars
+                            </span>
+                          </div>
+                          <nav className="flex flex-1 flex-col">
+                            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                              <li>
+                                <ul role="list" className="-mx-2 space-y-1">
+                                  {navigation.map((item) => (
+                                    <li key={item.name}>
+                                      <a
+                                        href={item.href}
+                                        className={classNames(
+                                          item.href === pathname
+                                            ? "bg-slate-900 text-green-500"
+                                            : "text-white hover:text-green-500 hover:bg-slate-900",
+                                          "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                                        )}
+                                      >
+                                        <item.icon
+                                          className="h-6 w-6 shrink-0"
+                                          aria-hidden="true"
+                                        />
+                                        {item.name}
+                                      </a>
+                                    </li>
+                                  ))}
+                                </ul>
+                              </li>
+                              <li>
+                                <div className="text-xs font-semibold leading-6 text-gray-400">
+                                  Your accounts
+                                </div>
+                                <ul role="list" className="-mx-2 mt-2 space-y-1">
+                                  {teams.map((team) => (
+                                    <li key={team.name}>
+                                      <a
+                                        href={team.href}
+                                        className={classNames(
+                                          team.href === pathname
+                                            ? "bg-slate-900 text-green-500"
+                                            : "text-white hover:text-green-500 hover:bg-slate-900",
+                                          "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                                        )}
+                                      >
+                                        <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-[#020617]">
+                                          {team.initial}
+                                        </span>
+                                        <span className="truncate">{team.name}</span>
+                                      </a>
+                                    </li>
+                                  ))}
+                                </ul>
+                              </li>
+                              <li className="-mx-6 mt-auto">
+                                {session ? (
+                                  <a
+                                    href="#"
+                                    className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 hover:bg-slate-900 text-slate-900"
+                                  >
+                                    <img
+                                      className="h-8 w-8 rounded-full bg-gray-800"
+                                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                      alt=""
+                                    />
+                                    <span className="sr-only">Your profile</span>
+                                    <span aria-hidden="true" className="text-white">
+                                      Nicholas Moreland
+                                    </span>
+                                  </a>
+                                ) : (
+                                  <div className="flex flex-row space-x-2 p-2">
+                                    <Button asChild className="w-full">
+                                      <Link href="/signup">Sign up</Link>
+                                    </Button>
+                                    <Button asChild className="w-full">
+                                      <Link href="/login">Log in</Link>
+                                    </Button>
+                                  </div>
+                                )}
+                              </li>
+                            </ul>
+                          </nav>
+                        </div>
+                      </div>
+
+                      <div className="sticky top-0 z-40 flex items-center gap-x-6 px-4 py-4 shadow-sm sm:px-6 lg:hidden border-b border-slate-900">
+                        <button
+                          type="button"
+                          className="-m-2.5 p-2.5 text-white lg:hidden"
+                          onClick={() => setSidebarOpen(true)}
                         >
-                          <item.icon
-                            className="h-6 w-6 shrink-0"
-                            aria-hidden="true"
+                          <span className="sr-only">Open sidebar</span>
+                          <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                        <div className="flex-1 text-sm font-semibold leading-6 text-white">
+                          Dashboard
+                        </div>
+                        <a href="#">
+                          <span className="sr-only">Your profile</span>
+                          <img
+                            className="h-8 w-8 rounded-full bg-gray-800"
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            alt=""
                           />
-                          {item.name}
                         </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-                <li>
-                  <div className="text-xs font-semibold leading-6 text-gray-400">
-                    Your accounts
-                  </div>
-                  <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                      <li key={team.name}>
-                        <a
-                          href={team.href}
-                          className={classNames(
-                            team.href === pathname
-                              ? "bg-slate-900 text-green-500"
-                              : "text-white hover:text-green-500 hover:bg-slate-900",
-                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                          )}
-                        >
-                          <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-slate-900">
-                            {team.initial}
-                          </span>
-                          <span className="truncate">{team.name}</span>
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-                <li className="-mx-6 mt-auto">
-                  {session ? (
-                    <a
-                      href="#"
-                      className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 hover:bg-slate-900 text-slate-900"
-                    >
-                      <img
-                        className="h-8 w-8 rounded-full bg-gray-800"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                      <span className="sr-only">Your profile</span>
-                      <span aria-hidden="true" className="text-white">
-                        Nicholas Moreland
-                      </span>
-                    </a>
-                  ) : (
-                    <div className="flex flex-row space-x-2 p-2">
-                      <Button asChild className="w-full">
-                        <Link href="/signup">Sign up</Link>
-                      </Button>
-                      <Button asChild className="w-full">
-                        <Link href="/login">Log in</Link>
-                      </Button>
+                      </div>
+
+                      <main className="py-10 lg:pl-72 2xl:pl-0 h-screen">
+                        <div className="px-4 sm:px-6 lg:px-8">{children}</div>
+                      </main>
                     </div>
-                  )}
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-
-        <div className="sticky top-0 z-40 flex items-center gap-x-6 px-4 py-4 shadow-sm sm:px-6 lg:hidden border-b border-slate-900">
-          <button
-            type="button"
-            className="-m-2.5 p-2.5 text-white lg:hidden"
-            onClick={() => setSidebarOpen(true)}
-          >
-            <span className="sr-only">Open sidebar</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-          <div className="flex-1 text-sm font-semibold leading-6 text-white">
-            Dashboard
-          </div>
-          <a href="#">
-            <span className="sr-only">Your profile</span>
-            <img
-              className="h-8 w-8 rounded-full bg-gray-800"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
-          </a>
-        </div>
-
-        <main className="py-10 lg:pl-72 2xl:pl-0 h-screen">
-          <div className="px-4 sm:px-6 lg:px-8">{children}</div>
-        </main>
-      </div>
-    </>
-  );
-}
+                  </>
+                );
+              }
